55fa49833a386d282b00bf14fd6089e3e10aa8fd
Aldycky Bagus Witjaksana <bagusaldycky@gmail.com>
2024-05-19 23:07:29 +0700

    refactor code

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -21,18 +21,18 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a
+                <router-link
                   v-for="item in navigation"
                   :key="item.name"
-                  :href="item.href"
+                  :to="item.to"
+                  active-class="bg-gray-900 text-white"
                   :class="[
-                    item.current
-                      ? 'bg-gray-900 text-white'
+                    this.$route.name === item.to.name
+                      ? ''
                       : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'rounded-md px-3 py-2 text-sm font-medium',
                   ]"
-                  :aria-current="item.current ? 'page' : undefined"
-                  >{{ item.name }}</a
+                  >{{ item.name }}</router-link
                 >
               </div>
             </div>
@@ -179,23 +179,22 @@ import {
   MenuItems,
 } from "@headlessui/vue";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+import { useStore } from "vuex";
+import { computed } from "vue";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
+  { name: "Dashboard", to: { name: "Dashboard" } },
+  { name: "Surveys", to: { name: "Surveys" } },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
   { name: "Sign out", href: "#" },
 ];
+
+const store = useStore();
+
+const user = () => {
+  computed(() => store.state.user.data);
+};
 </script>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,7 @@ import Surveys from "../views/Surveys.vue";
 import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import store from "../store";
 
 const routes = [
@@ -29,6 +30,8 @@ const routes = [
     path: "/auth",
     redirect: "/login",
     name: "Auth",
+    component: AuthLayout,
+    meta: { isGuest: true },
     children: [
       {
         path: "/login",
@@ -52,10 +55,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !store.state.user.token) {
     next({ name: "Login" });
-  } else if (
-    store.state.user.token &&
-    (to.name === "Login" || to.name === "Register")
-  ) {
+  } else if (store.state.user.token && to.meta.isGuest) {
     next({ name: "Dashboard" });
   } else {
     next();
